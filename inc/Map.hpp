a99344c8a6f630c47565601377f2536d5c8b0af7
ezagof_home <ezagof@gmail.com>
2024-12-22 21:01:50 +0100

    add new and delete grid_layer template functions

--- inc/Map.hpp
+++ inc/Map.hpp
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:54:10 by aautin            #+#    #+#             */
-/*   Updated: 2024/12/22 16:31:59 by aautin           ###   ########.fr       */
+/*   Updated: 2024/12/23 02:42:49 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ class Map
 		size_t	get_height() const;
 		Cell&	get_cell(size_t x, size_t y) const;
 		Stock&	get_stock(e_owner index);
+
+		//Grid layer
+		template <typename Type> Type**	new_grid_layer(Type value) const;
+		template <typename Type> void	delete_grid_layer(Type** grid_layer) const;
 
 	private:
 		size_t const	_grid_width;
--- src/Map.cpp
+++ src/Map.cpp
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 12:35:41 by aautin            #+#    #+#             */
-/*   Updated: 2024/12/22 17:48:34 by aautin           ###   ########.fr       */
+/*   Updated: 2024/12/23 02:43:44 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ Map::Map(size_t width, size_t height)
 : _grid_width(width), _grid_height(height), _stocks{Stock(), Stock()}
 {
 	_grid = new Cell*[width];
-	for (size_t i = 0; i < width; ++i)
-		_grid[i] = new Cell[height];
+	for (size_t x = 0; x < width; ++x)
+		_grid[x] = new Cell[height];
 }
 
 Map::~Map()
 {
-	for (size_t i = 0; i < _grid_width; ++i)
-		delete _grid[i];
+	for (size_t x = 0; x < _grid_width; ++x)
+		delete _grid[x];
 	delete _grid;
 }
 //-
@@ -55,5 +55,27 @@ void	Map::set_cell(Cell cell, size_t x, size_t y)
 	if (x >= _grid_width || y >= _grid_height)
 		throw std::out_of_range("");
 	_grid[x][y] = cell;
+}
+//-
+
+
+//Grid layer
+template <typename Type>
+Type**	Map::new_grid_layer(Type value) const
+{
+	Type** grid_layer = new Type*[_grid_width];
+	for (size_t x = 0; x < _grid_width; ++x) {
+		grid_layer[x] = new Type[_grid_height];
+		for (size_t y = 0; y < _grid_height; ++y)
+			grid_laye[x][y] = value;
+	}
+	return grid_layer;
+}
+
+template <typename Type> void	Map::delete_grid_layer(Type** grid_layer) const
+{
+	for (size_t x = 0; x < _grid_width; ++x)
+		delete grid_layer[x];
+	delete grid_layer;
 }
 //-
